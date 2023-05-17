c512e4463ddc35a3bb1a8e1d2690222bf5cfa326
Pragyanand Sahoo <pragyanandasaho@gmail.com>
2023-05-17 08:52:16 +0000

    add static folder

--- rscurry/settings.py
+++ rscurry/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS =[
-    BASE_DIR,"static/"
+    BASE_DIR,"static"
 ]
 
 MEDIA_ROOT =BASE_DIR /"media"
